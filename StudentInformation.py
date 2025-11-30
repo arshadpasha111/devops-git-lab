9b54afed3e3299981ec4f811a4321e6c833b4057
arshadpasha111 <31982859+arshadpasha111@users.noreply.github.com>
2025-11-30 13:57:07 +0530

    Create StudentInformation.py
    Student Details

---
+++ StudentInformation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Script: Student Information
+
+def main():
+    student_name = "Ram Kumar"
+    roll_number = "909414261"
+    college_name = "Hindi College Moradabad"
+    admission_year = 2009
+
+    print("===== Student Details =====")
+    print("Name           :", student_name)
+    print("Roll Number    :", roll_number)
+    print("College Name   :", college_name)
+    print("Admission Year :", admission_year)
+
+if __name__ == "__main__":
+    main()
